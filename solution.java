80803b1723769c6a42939a96aa0b896c9ca949a9
JQUAN COLLINS <104362301+jquan114@users.noreply.github.com>
2023-03-22 20:52:29 -0400

    Create solution.java

---
+++ solution.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        // get squares of the first 5 integers
+
+        System.out.println( 1 + " squared = " + (1*1));
+        System.out.println( 2 + " squared = " + (2*2));
+        System.out.println( 3 + " squared = " + (3*3));
+        System.out.println( 4 + " squared = " + (4*4));
+        System.out.println( 5 + " squared = " + (5*5));
+
+    }
+}
